18fe4636a56452aa8124c27b456b83f09649ef1b
hp <nagababupenke14@gmail.com>
2026-02-19 19:51:03 +0530

    register page input fields created

--- hms_frontend/src/components/Registerpage.jsx
+++ hms_frontend/src/components/Registerpage.jsx
@@ -1,8 +1,27 @@
-
+import "../css/Registerpage.css"
 function Registerpage(){
     return (
         <div className="registerpage">
-            <h1>register page</h1>
+            <label htmlFor="">
+                <input type="text" placeholder="ENTER USER ID"/>
+            </label><br /><br />
+            <label htmlFor="">
+                <input type="text" placeholder="ENTER USERNAME"/>
+            </label><br /><br />
+            <label htmlFor="">
+                <input type="email" placeholder="ENTER EMAIL ID"/>
+            </label><br /><br />
+            <label htmlFor="">
+                <input type="password" placeholder="CREATE PASSWORD"/>
+            </label><br /><br />
+            <label htmlFor="">
+                <select name="" id="" defaultValue={'SELECT ROLE'}>
+                    <option value="DOCTOR">DOCTOR</option>
+                    <option value="PATIENT">PATIENT</option>
+                    <option value="RECEPTIONIST">RECEPTIONIST</option>
+                </select>
+            </label><br /><br />
+            <button>REGISTER</button>
         </div>
     );
 }
